886819e861a7642d22fbe8f43668d95d16ba2810
Rails udemy <rails@gmail.com>
2017-04-14 18:26:24 +0000

    created sumthng new

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 root 'welcome#home'
 get 'about', to: 'welcome#about'
+resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
