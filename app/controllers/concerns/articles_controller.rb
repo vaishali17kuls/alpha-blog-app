e73cfe705c680f02b31581622779a8e42c9f6afe
Rails udemy <rails@gmail.com>
2017-04-15 06:05:57 +0000

    added all d crud operations

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -3,10 +3,47 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def index
+   @articles = Article.all  #grabs ol d articles in @article variable
+  end
+  
+  def edit
+     @article = Article.find(params[:id])
+  end
+  
+  def update
+     @article = Article.find(params[:id])
+     if @article.update(article_params)
+       flash[:notice] = "Article was successfully updated"
+       redirect_to article_path(@article)
+     else
+     render 'edit'
+     end
+  end
+  
   def create
     #render plain: params[:article].inspect
     @article=Article.new(article_params)
-    @article.save  #saves d article created but doesnt know where to redirect after dat
+     if @article.save
+        flash[:notice] = "Article was successfully created"
+        redirect_to article_path(@article)
+
+     else
+    render 'new'
+
+     end
+    #@article.save  #saves d article created but doesnt know where to redirect after dat
+  end
+  
+  def show
+   @article = Article.find(params[:id])  #find article id and show it
+  end
+  
+  def destroy
+   @article = Article.find(params[:id])
+   @article.destroy
+   flash[:notice] = "Article was successfully deleted"
+   redirect_to articles_path
   end
   
   private
