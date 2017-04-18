8dfcf6dfe7c40ca0750fc498200eb53587e67453
Rails udemy <rails@gmail.com>
2017-04-18 06:11:58 +0000

    signup made

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 root 'welcome#home'
 get 'about', to: 'welcome#about'
 resources :articles
+get 'signup', to: 'users#new'
+resources :users, except: [:new]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
