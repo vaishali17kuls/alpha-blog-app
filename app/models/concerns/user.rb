2939fb54314523c2138e45867873eaa2fe71cac1
Rails udemy <rails@gmail.com>
2017-04-18 05:38:48 +0000

    added pswd

--- app/models/concerns/user.rb
+++ app/models/concerns/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   validates :username, uniqueness: { case_sensitive: false }, presence: true, length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, uniqueness: { case_sensitive: false }, presence: true, length: { minimum: 3, maximum: 25 }, format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
